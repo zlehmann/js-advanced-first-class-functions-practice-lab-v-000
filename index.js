d78e8a6b7e312b0ce69be4d9c4947b4158caaf44
Gabe Jackson <gj@mail.co.de>
2017-09-29 15:26:47 -0400

    Remove arrow functions; use const instead of let; make solutions nondestructive

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 // Code your solution in this file!
 
-function logDriverNames(drivers){
-  drivers.forEach((driver)=> {
-    console.log(driver.name)
-  })
-}
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+};
 
-function logDriversByHometown(drivers, hometown){
-  drivers.forEach((driver)=> {
-    if(driver.hometown == hometown){
-      console.log(driver.name)
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
     }
-  })
-}
+  });
+};
 
-function driversByRevenue(drivers){
-  let sorted = drivers.sort((driverOne, driverTwo) => {
-    return driverOne.revenue - driverTwo.revenue
-  })
-  return sorted
-}
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
 
-function driversByName(drivers){
-  let sorted = drivers.sort((driverOne, driverTwo) => {
-    return driverOne.name.localeCompare(driverTwo.name)
-  })
-  return sorted
-}
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
 
-function totalRevenue(drivers){
-  return drivers.reduce(function(previous, currentDriver){
-    return currentDriver.revenue + previous
-  }, 0)
-}
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
 
-function averageRevenue(drivers){
-  return totalRevenue(drivers) / drivers.length
-}
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
